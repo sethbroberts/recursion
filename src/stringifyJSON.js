eb0eda77704dd94dfcf70030f963cd961386ceb2
Seth Roberts <seth.b.roberts (at) gmail (dot) com>
2015-08-26 14:20:34 -0400

    implemented for primitives and arrays

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,43 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+
+  // is obj a primitive (undefined, null, string, boolean, number)?
+  // if yes, return stringified version
+  // if it is number, string, true/false, undefined, null, return String(obj)
+  if (typeof obj === "number" || 
+  	typeof obj === "boolean" ||
+  	obj === null ||
+  	obj === undefined) {
+  	return String(obj);
+  } else if (typeof obj === "string") {
+  	return '"' + obj + '"';
+  }
+  
+  // is obj an array?
+  // return '[' + stringifyJSON(item) + ']' for each item in array, like this:
+  // result = '['
+  // forEach (item) in obj:
+  //   result = result + stringifyJSON(item);
+  // return result + ']';
+  if (obj.constructor === Array) {
+  	var result = '[';
+  	if (obj.length > 0) {
+  		for (var i=0; i<obj.length; i++) {
+  			result = result + stringifyJSON(obj[i]) + ',';
+  		}
+  		result = result.slice(0, -1);
+  	}
+  	return result + ']';
+  }
+ 
+  // is obj an object?
+  // if yes, return '{' + stringifyJSON(item) + '}' for each item in object:
+  // result = '{'
+  // forEach (item) in obj:
+  //   result = result + stringifyJSON(item);
+  // return result + '}';
+
+
+
 };
