3c2fd56868a4ac64a69bb1bf2cb9371386e3bc99
Seth Roberts <seth.b.roberts (at) gmail (dot) com>
2015-08-26 17:36:03 -0400

    stringifyJSON implemented, passing tests

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,21 +3,33 @@
 
 // but you don't so you're going to write it from scratch:
 
+function isLegal(object) {
+	if (object === null) {		// check this first:
+		return true;			// if call null.constructor below, get error
+	} else if (object === undefined) {
+		return false;
+	} else if (object.constructor === Function) {
+		return false;
+	}
+	return true;
+}
+
 var stringifyJSON = function(obj) {
   // your code goes here
 
-  // is obj a primitive (undefined, null, string, boolean, number)?
+  // is obj a primitive (null, string, boolean, number)?
   // if yes, return stringified version
-  // if it is number, string, true/false, undefined, null, return String(obj)
+  // if it is number, string, true/false, null, return String(obj)
   if (typeof obj === "number" || 
   	typeof obj === "boolean" ||
-  	obj === null ||
-  	obj === undefined) {
+  	obj === null) {
   	return String(obj);
   } else if (typeof obj === "string") {
   	return '"' + obj + '"';
+  } else if (obj === undefined) {		// if undefined, return nothing
+  	return;
   }
-  
+
   // is obj an array?
   // return '[' + stringifyJSON(item) + ']' for each item in array, like this:
   // result = '['
@@ -28,9 +40,13 @@ var stringifyJSON = function(obj) {
   	var result = '[';
   	if (obj.length > 0) {
   		for (var i=0; i<obj.length; i++) {
-  			result = result + stringifyJSON(obj[i]) + ',';
+  			// make sure obj[key] is not undefined or a function
+  			if (isLegal(obj[i])) {
+  				result = result + stringifyJSON(obj[i]) + ',';
+  			}
+  			
   		}
-  		result = result.slice(0, -1);
+  		result = result.slice(0, -1);	// take off trailing comma
   	}
   	return result + ']';
   }
@@ -41,7 +57,21 @@ var stringifyJSON = function(obj) {
   // forEach (item) in obj:
   //   result = result + stringifyJSON(item);
   // return result + '}';
-
-
+  if (obj.constructor === Object) {
+  	var result = '{';
+  	var empty = true;
+	for (var key in obj) {
+		// make sure obj[key] is not undefined or a function
+		if (isLegal(obj[key])) {
+			empty = false;
+			result = result + stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',';
+		}
+	}
+	if (!empty) {
+		result = result.slice(0, -1);	// take off trailing comma
+	}
+  	return result + '}';
+  	
+  }
 
 };
